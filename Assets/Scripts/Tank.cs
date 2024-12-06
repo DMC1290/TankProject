5c126d95c983051a8432a65b7574364874b06f29
DMC1290 <davideM.98@hotmail.com>
2024-12-06 18:19:16 +0100

    marche arriere

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -4,9 +4,11 @@ using UnityEngine.InputSystem;
 public class TankController : MonoBehaviour
 {
     [SerializeField] private float _force = 200.0f;
+    [SerializeField] private float _reverse_force = -200.0f;
     [SerializeField] private float _turnSpeed = 30f;
 
     private bool _boosterInput = false;
+    private bool _reverse_boostInput = false;
     private float _turnInput = 0f;
 
     private Rigidbody _rigidbody;
@@ -31,6 +33,11 @@ public class TankController : MonoBehaviour
             _rigidbody.AddForce( transform.forward * _force);
         }
 
+        if (_reverse_boostInput)
+        {
+            _rigidbody.AddForce(transform.forward * _reverse_force);
+        }
+
         _rigidbody.AddRelativeTorque(0, _turnInput * _turnSpeed, 0);
         Debug.Log(_turnInput);
     }
@@ -38,6 +45,11 @@ public class TankController : MonoBehaviour
     void OnBoost(InputValue value)
     {
         _boosterInput = value.isPressed;
+    }
+
+    void OnReverseBoost(InputValue value)
+    {
+        _reverse_boostInput = value.isPressed;
     }
 
     void OnLeftRight(InputValue value)
